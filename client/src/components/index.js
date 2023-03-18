a1a26c6c0140c354fd63d708a8c743124ddcbbcb
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-18 22:05:47 +0800

    feat: add product card component, add to showcase
    https://chakra-templates.dev/components/cards

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,7 @@
 import Nav from "./Nav";
 import Hero from "./Hero";
 import Showcase from "./Showcase";
+import CardProduct from "./CardProduct";
 import CardSignIn from "./CardSignIn";
 import CardSignUp from "./CardSignup";
 
@@ -8,6 +9,7 @@ export {
     Nav,
     Hero,
     Showcase,
+    CardProduct,
     CardSignIn,
     CardSignUp,
 };
