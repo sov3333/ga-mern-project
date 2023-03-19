a96db26091f6ad0d14c93715d6d5b37894852336
deeba <adibah@hotmail.sg>
2023-03-19 18:56:44 +0800

    change userSchema format

--- server/mongodb/models/user.js
+++ server/mongodb/models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   first_name: { type: String, required: true },
@@ -10,4 +10,4 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+export default { User };
