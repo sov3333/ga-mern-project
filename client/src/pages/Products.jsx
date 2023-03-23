03b9d6f2d49fc8e8212b155e18773b199a1217b9
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-03-24 01:47:45 +0800

    Add in router.post for type/review and testing out product.jsx to display data

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -5,6 +5,16 @@ import { productsData } from '../constants';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [username, setUsername] = useState('');
+  const [review, setReview] = useState('');
+  const [showReviews, setShowReviews] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add review to database
+    setUsername('');
+    setReview('');
+  };
 
   useEffect(() => {
     fetch('http://localhost:8080/api/product')
@@ -93,6 +103,55 @@ const Products = () => {
               model={item.model}
               slug={`/products/${item.type}`}
             />
+          </div>
+        ))}
+      </div>
+      <div>
+        {products.map((product) => (
+          <div key={product._id}>
+            <h2>
+              {product.brand} {product.model}
+            </h2>
+            <img src={product.img} alt={product.model} />
+            <p>Type: {product.type}</p>
+            <p>
+              Rating:{' '}
+              {product.ratings
+                .map((rating) => rating.rating)
+                .reduce((a, b) => a + b, 0) / product.ratings.length}{' '}
+              out of 5
+            </p>
+            <p>Number of Reviews: {product.reviews.length}</p>
+            <button onClick={() => setShowReviews(!showReviews)}>
+              {showReviews ? 'Hide Reviews' : 'Show Reviews'}
+            </button>
+            {showReviews && (
+              <ul>
+                {product.reviews.map((review) => (
+                  <li key={review._id}>
+                    {review.user}: {review.review}
+                  </li>
+                ))}
+              </ul>
+            )}
+            <form onSubmit={handleSubmit}>
+              <label htmlFor='username'>Username:</label>
+              <input
+                type='text'
+                id='username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <br />
+              <label htmlFor='review'>Review:</label>
+              <textarea
+                id='review'
+                value={review}
+                onChange={(e) => setReview(e.target.value)}
+              ></textarea>
+              <br />
+              <button type='submit'>Add Review</button>
+            </form>
           </div>
         ))}
       </div>
