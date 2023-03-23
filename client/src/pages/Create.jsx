b6bdbed81f416ab491e1b49ec95fe9c3d71ba5b1
deeba <adibah@hotmail.sg>
2023-03-23 13:53:21 +0800

    connecting POST route between BE  and FE 'Create' page

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -13,6 +13,7 @@ import {
   useBreakpointValue,
   Icon,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 // "Join our Team" from https://chakra-templates.dev/forms/authentication
 
@@ -40,6 +41,33 @@ const avatars = [
 ];
 
 export default function Create() {
+  const [newSetup, setNewSetup] = useState('');
+
+  const [newTitle, setNewTitle] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const [newProducts, setNewProducts] = useState('');
+  const [newPhoto, setNewPhoto] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:8080/api/setup', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: 'Alice',
+        title: newTitle,
+        description: newDescription,
+        products: newProducts,
+        img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ7Y3DJGIZejTxIdwspZPJhd40NKcYKHKXbGuoH7MxteDaJZJQl',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setNewSetup(data.newSetup))
+      .catch((err) => console.error({ Error: err }));
+  };
+
   return (
     <Box position={'relative'}>
       <Container
@@ -47,16 +75,19 @@ export default function Create() {
         maxW={'7xl'}
         columns={{ base: 1, md: 2 }}
         spacing={{ base: 10, lg: 32 }}
-        py={{ base: 10, sm: 20, lg: 32 }}>
+        py={{ base: 10, sm: 20, lg: 32 }}
+      >
         <Stack spacing={{ base: 10, md: 20 }}>
           <Heading
             lineHeight={1.1}
-            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}>
+            fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}
+          >
             Showcase your style{' '}
             <Text
               as={'span'}
-              bgGradient="linear(to-r, red.400,pink.400)"
-              bgClip="text">
+              bgGradient='linear(to-r, red.400,pink.400)'
+              bgClip='text'
+            >
               &
             </Text>{' '}
             inspire productivity
@@ -111,7 +142,8 @@ export default function Create() {
                 zIndex: -1,
                 top: 0,
                 left: 0,
-              }}>
+              }}
+            >
               YOU
             </Flex>
           </Stack>
@@ -121,28 +153,41 @@ export default function Create() {
           rounded={'xl'}
           p={{ base: 4, sm: 6, md: 8 }}
           spacing={{ base: 8 }}
-          maxW={{ lg: 'lg' }}>
+          maxW={{ lg: 'lg' }}
+        >
           <Stack spacing={4}>
             <Heading
               color={'gray.800'}
               lineHeight={1.1}
-              fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}>
+              fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}
+            >
               Share Your Setup
               <Text
                 as={'span'}
-                bgGradient="linear(to-r, red.400,pink.400)"
-                bgClip="text">
+                bgGradient='linear(to-r, red.400,pink.400)'
+                bgClip='text'
+              >
                 !
               </Text>
             </Heading>
             <Text color={'gray.500'} fontSize={{ base: 'sm', sm: 'md' }}>
-              Join our community of desk setup enthusiasts and share your own trading gaming or programming workspace. Upload a photo of your setup, add a catchy title and a brief description, and let others rate and comment on your creation. Whether you're a minimalist or a tech enthusiast, we welcome all setups and styles. Share your setup with the world and inspire others to upgrade their own desks!
+              Join our community of desk setup enthusiasts and share your own
+              trading gaming or programming workspace. Upload a photo of your
+              setup, add a catchy title and a brief description, and let others
+              rate and comment on your creation. Whether you're a minimalist or
+              a tech enthusiast, we welcome all setups and styles. Share your
+              setup with the world and inspire others to upgrade their own
+              desks!
             </Text>
           </Stack>
           <Box as={'form'} mt={10}>
             <Stack spacing={4}>
               <Input
-                placeholder="Title e.g. Productivity Haven"
+                value={newTitle}
+                onChange={(e) => {
+                  setNewTitle(e.target.value);
+                }}
+                placeholder='Title e.g. Productivity Haven'
                 bg={'gray.100'}
                 border={0}
                 color={'gray.500'}
@@ -151,7 +196,11 @@ export default function Create() {
                 }}
               />
               <Input
-                placeholder="Describe your setup"
+                value={newDescription}
+                onChange={(e) => {
+                  setNewDescription(e.target.value);
+                }}
+                placeholder='Describe your setup'
                 bg={'gray.100'}
                 border={0}
                 color={'gray.500'}
@@ -160,7 +209,12 @@ export default function Create() {
                 }}
               />
               <Input
-                placeholder="desk, monitor, keyboard, mouse"
+                value={newProducts}
+                onChange={(e) => {
+                  setNewProducts(e.target.value);
+                }}
+                //type='Products'
+                placeholder='desk, monitor, keyboard, mouse'
                 bg={'gray.100'}
                 border={0}
                 color={'gray.500'}
@@ -173,15 +227,17 @@ export default function Create() {
               </Button>
             </Stack>
             <Button
+              onClick={handleSubmit}
               fontFamily={'heading'}
               mt={8}
               w={'full'}
-              bgGradient="linear(to-r, red.400,pink.400)"
+              bgGradient='linear(to-r, red.400,pink.400)'
               color={'white'}
               _hover={{
                 bgGradient: 'linear(to-r, red.400,pink.400)',
                 boxShadow: 'xl',
-              }}>
+              }}
+            >
               Submit
             </Button>
           </Box>
@@ -203,18 +259,19 @@ export const Blur = (props) => {
     <Icon
       width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
       zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
-      height="560px"
-      viewBox="0 0 528 560"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}>
-      <circle cx="71" cy="61" r="111" fill="#F56565" />
-      <circle cx="244" cy="106" r="139" fill="#ED64A6" />
-      <circle cy="291" r="139" fill="#ED64A6" />
-      <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
-      <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
-      <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
-      <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
+      height='560px'
+      viewBox='0 0 528 560'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <circle cx='71' cy='61' r='111' fill='#F56565' />
+      <circle cx='244' cy='106' r='139' fill='#ED64A6' />
+      <circle cy='291' r='139' fill='#ED64A6' />
+      <circle cx='80.5' cy='189.5' r='101.5' fill='#ED8936' />
+      <circle cx='196.5' cy='317.5' r='101.5' fill='#ECC94B' />
+      <circle cx='70.5' cy='458.5' r='101.5' fill='#48BB78' />
+      <circle cx='426.5' cy='-0.5' r='101.5' fill='#4299E1' />
     </Icon>
   );
 };
