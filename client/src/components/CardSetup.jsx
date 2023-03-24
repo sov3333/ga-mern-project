e921ccba3964b46c3b5a9136b5a6bbd74d26e10c
deeba <adibah@hotmail.sg>
2023-03-25 00:11:15 +0800

    displaying setup DB  in setups page

--- client/src/components/CardSetup.jsx
+++ client/src/components/CardSetup.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+//import { Link } from 'react-router-dom';
 import {
   Box,
   Center,
@@ -12,10 +13,24 @@ import {
 
 // "Product Simple" from https://chakra-templates.dev/components/cards
 
-export default function CardSetup({ img, user, title, description, products, slug }) {
+export default function CardSetup() {
+  const [setups, setSetups] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/setup')
+      .then(
+        (data) => data.json(),
+        (err) => console.log(err)
+      )
+      .then(
+        (parsedData) => setSetups(parsedData),
+        (err) => console.log(err)
+      );
+  }, []);
+
   return (
     <Center py={12}>
-      <Link 
+      {/* <Link 
         to={slug}
         state={{
           img: img,
@@ -24,85 +39,115 @@ export default function CardSetup({ img, user, title, description, products, slu
           description: description,
           products: products,
         }}
-      >
-      <Box
-        role={'group'}
-        p={6}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${img})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
+      > */}
+      {setups.map((setup) => {
+        return (
+          <Box
+            key={setup._id}
+            role={'group'}
+            p={6}
+            maxW={'330px'}
+            w={'full'}
+            bg={useColorModeValue('white', 'gray.800')}
+            boxShadow={'2xl'}
             rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'cover'}
-            src={img}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            @{user}
-          </Text>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            {title}
-          </Heading>
-          <Stack direction={'row'} align={'center'}>
-            {/* <Text fontWeight={800} fontSize={'xl'}> */}
-              <Wrap>
-                {products.map((item, index) => (
-                  <Text key={index} fontWeight={400} fontSize={'sm'} color={
-                    // change color of #hashtag per product
-                    item === 'desk' ? 'teal' :
-                    item === 'monitor' ? 'green' :
-                    item === 'chair' ? 'orange' :
-                    item === 'keyboard' ? 'peru' :
-                    item === 'mouse' ? 'red' :
-                    item === 'mousepad' ? 'aqua' :
-                    item === 'speaker' ? 'blue' :
-                    item === 'headphone' ? 'lightgreen' :
-                    item === 'pc' ? 'purple' :
-                    item === 'laptop' ? 'pink' :
-                    item === 'light' ? 'plum' :
-                    item === 'riser' ? 'tomato' :
-                    'black' // fallback color
-                  }>#{item}</Text>
-                ))}
-              </Wrap>
-              
-            {/* </Text> */}
-            {/* <Text textDecoration={'line-through'} color={'gray.600'}>
+            pos={'relative'}
+            zIndex={1}
+          >
+            <Box
+              rounded={'lg'}
+              mt={-12}
+              pos={'relative'}
+              height={'230px'}
+              _after={{
+                transition: 'all .3s ease',
+                content: '""',
+                w: 'full',
+                h: 'full',
+                pos: 'absolute',
+                top: 5,
+                left: 0,
+                backgroundImage: `url(${setup.img})`,
+                filter: 'blur(15px)',
+                zIndex: -1,
+              }}
+              _groupHover={{
+                _after: {
+                  filter: 'blur(20px)',
+                },
+              }}
+            >
+              <Image
+                rounded={'lg'}
+                height={230}
+                width={282}
+                objectFit={'cover'}
+                src={setup.img}
+              />
+            </Box>
+            <Stack pt={10} align={'center'}>
+              <Text
+                color={'gray.500'}
+                fontSize={'sm'}
+                textTransform={'uppercase'}
+              >
+                @{setup.user}
+              </Text>
+              <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+                {setup.title}
+              </Heading>
+              <Stack direction={'row'} align={'center'}>
+                {/* <Text fontWeight={800} fontSize={'xl'}> */}
+                <Wrap>
+                  {setup.products.map((item, index) => (
+                    <Text
+                      key={index}
+                      fontWeight={400}
+                      fontSize={'sm'}
+                      color={
+                        // change color of #hashtag per product
+                        item === 'desk'
+                          ? 'teal'
+                          : item === 'monitor'
+                          ? 'green'
+                          : item === 'chair'
+                          ? 'orange'
+                          : item === 'keyboard'
+                          ? 'peru'
+                          : item === 'mouse'
+                          ? 'red'
+                          : item === 'mousepad'
+                          ? 'aqua'
+                          : item === 'speaker'
+                          ? 'blue'
+                          : item === 'headphone'
+                          ? 'lightgreen'
+                          : item === 'pc'
+                          ? 'purple'
+                          : item === 'laptop'
+                          ? 'pink'
+                          : item === 'light'
+                          ? 'plum'
+                          : item === 'riser'
+                          ? 'tomato'
+                          : 'black' // fallback color
+                      }
+                    >
+                      #{item}
+                    </Text>
+                  ))}
+                </Wrap>
+
+                {/* </Text> */}
+                {/* <Text textDecoration={'line-through'} color={'gray.600'}>
               $199
             </Text> */}
-          </Stack>
-        </Stack>
-      </Box>
-      </Link>
+              </Stack>
+            </Stack>
+          </Box>
+        );
+      })}
+      {/* </Link> */}
     </Center>
   );
 }
