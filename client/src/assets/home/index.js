f7b9e4dcbb24239d9178f31811fb6d715c71a501
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-24 10:44:27 +0800

    feat: add About section, TypingText component

--- client/src/assets/home/index.js
+++ client/src/assets/home/index.js
@@ -2,10 +2,12 @@ import search from './search.svg';
 import menu from './menu.svg';
 import cover from './cover.jpg';
 import stamp from './stamp.png';
+import arrow_down from './arrow-down.svg';
 
 export {
     search,
     menu,
     cover,
     stamp,
+    arrow_down,
 };
