6ff945afcb59662d5c369625275cdc0bc312f6f5
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-24 10:35:21 +0800

    feat: add hero section

--- client/src/assets/home/index.js
+++ client/src/assets/home/index.js
@@ -1,7 +1,11 @@
 import search from './search.svg';
 import menu from './menu.svg';
+import cover from './cover.jpg';
+import stamp from './stamp.png';
 
 export {
     search,
     menu,
+    cover,
+    stamp,
 };
