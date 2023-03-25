abdd473cc79e69e45ac0f6365675de8bad9d073d
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-25 13:19:07 +0800

    feat: update new navbar w all buttons/links

--- client/src/components/home/Navbar.jsx
+++ client/src/components/home/Navbar.jsx
@@ -1,34 +1,64 @@
+import { useState } from 'react';
 import { motion } from 'framer-motion';
+import { Avatar, Button, IconButton, Link } from '@chakra-ui/react';
+import { AddIcon, ViewIcon } from '@chakra-ui/icons';
 
 import styles from '../../styles';
-import { navVariants } from '../../utils/motion';
-import { search, menu } from '../../assets/home'
+import NavMenu from './NavMenu';
 
-const Navbar = () => (
-  <motion.nav
-    variants={navVariants}
-    initial="hidden"
-    whileInView="show"
-    viewport={{ once: true }}
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img
-        src={search}
-        alt="search"
-        className="w-[24px] h-[24px] object-contain"
-      />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-        SWIPE SETUPS
-      </h2>
-      <img
-        src={menu}
-        alt="menu"
-        className="w-[24px] h-[24px] object-contain"
-      />
-    </div>
-  </motion.nav>
-);
+const Navbar = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+
+  return (
+    <nav
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      {/* // <div className="absolute w-[50%] inset-0 gradient-01 z-0" />  */}
+      {/* TODO / BUG: gradient is blocking some parts of nav from being clickable even though added z-0 to gradient, and higher z e.g. z-10 to items i want to click. Commented out gradient for now  */}
+      {/* https://tailwindcss.com/docs/z-index */}
+      <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8 z-50`}>
+        <div>
+          <NavMenu />
+        </div>
+        <Link href="/">
+          <h2 className="font-extrabold text-[24px] leading-[30px] text-white z-50">
+            SWIPE SETUPS
+          </h2>
+        </Link>
+        <div className="flex items-center">
+          {loggedIn ? (
+            <>
+            {/* TODO / BUG: remove underline from text when hover over button  */}
+            <Link href="/create">
+              <Button size="sm" colorScheme="green" leftIcon={<AddIcon />}>
+                Post
+              </Button>
+            </Link>
+            <Link href="/profile">
+              {/* https://chakra-ui.com/docs/components/avatar#avatar-fallbacks */}
+              {/* If name prop is passed to Avatar, default image will be replaced with initials and random bg color */}
+              <Avatar name={""} size="sm" className="ml-5" />
+            </Link>
+            </>
+          ) : (
+            <>
+            <Link href="/signin">
+              <p className="text-white mr-5 font-bold no-underline sm:block hidden">
+                Sign In
+              </p>
+            </Link>
+            {/* TODO / BUG: remove underline from text when hover over button  */}
+            <Link href="/signup" className="no-underline">
+              <Button size="sm" colorScheme="pink">Sign Up</Button>
+            </Link>
+            </>
+          )}
+        </div>
+        {/* TEMPORARY - Toggle button to see SignIn / SignOut state */}
+        <IconButton icon={<ViewIcon />} size="sm" onClick={() => setLoggedIn(!loggedIn)} />
+      </div>
+    </nav>
+  )
+};
 
 export default Navbar;
