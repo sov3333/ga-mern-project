e1631ce4c1b2af6f227a531dcf82a99eee6c6e76
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-28 21:42:18 +0800

    feat: attempt filter setups user already swiped
    bugged: doesn't work on page load. but after loading page, save the Swipe.jsx file in code editor, and should see frontend update with some setups removed from the balanceSetups.

--- client/src/pages/Swipe.jsx
+++ client/src/pages/Swipe.jsx
@@ -68,9 +68,51 @@ const Swipe = () => {
         // create temp array to store all setups
         let tempSetupsArray = parsedData;
 
+        // look through the tempSwipesArray
+        // if userId === one of the tempSwipesArray[i].userId 
+        // means user already liked/disliked this item
+        // so remove this item from the array
+        // then re-roll the array
+        // console.log(`tempSetupsArray`, tempSetupsArray);
+        console.log(`parsedData1111`, parsedData);
+
+        console.log(`userId`, userId)
+
+        // BUG / TODO:
+        // trying to filter away setups that user already swiped.
+        // doesn't work on page load. but after loading page, save the Swipe.jsx file in code editor, and should see frontend update with some setups removed from the balanceSetups.
+      
+        // loop through the parsedData (all setups)
+        for (let i=0; i<parsedData.length; i++) {
+          console.log(`in the loop, ${parsedData[i]}`)
+          // for each indiivdual setup, loop through the array for all swipes
+          for (let j=0; j<parsedData[i].swipes.length; j++) {
+            // check if the currently logged in userId === the userId in one of the swiped items
+            if (userId === parsedData[i].swipes[j].userId) {
+              console.log(`found that this setup ${parsedData[i].title} is already swiped!`)
+              // remove this item from the tempSetupsArray
+              console.log(`gonna remove this item`, tempSetupsArray[i].swipes[j]);
+              console.log(`gonna remove this setup`, tempSetupsArray[i]);
+              // tempSetupsArray[i].swipes.splice(j);
+              console.log(`removedSwipe:`, tempSetupsArray.splice(i, 1));
+
+              break;
+
+            }
+          }
+        }
+
+        console.log(`tempSetupsArray after for loop spaghetti`, tempSetupsArray);
+
         // randomly pick 1 setup (removes item from tempArray)
         let randomIndex = Math.floor(Math.random() * parsedData.length);
         let removedItem = tempSetupsArray.splice(randomIndex, 1); // returns array with 1 item
+
+        console.log(`parsedData selectedItem`, removedItem);
+
+
+        console.log(`parsedData selectedItem[0].swipes`, removedItem[0].swipes);
+
 
         // store randomly picked item as currentSetup to show on UI
         setCurrentSetup(removedItem[0]);
@@ -94,7 +136,9 @@ const Swipe = () => {
   // handle user click like button or swipe right on mobile
   const handleLiked = (bool) => {
 
-    if (balanceSetups.length > 0) {      
+    console.log(`Swiped Setup`, currentSetup);
+    
+    if (balanceSetups.length > 0) {
 
       // update setup in db
       fetch(`http://localhost:8080/api/setup/swipe/${currentSetup._id}`, {
