fa17698bd6d3688cd0b549fae7e964da88db1382
weilianglee87 <weiliang.gsmedia@gmail.com>
2023-03-30 01:17:31 +0800

    add title and setup into the swipe page

--- client/src/pages/Swipe.jsx
+++ client/src/pages/Swipe.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { Container } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react';
+import { Container } from '@chakra-ui/react';
 
-import { ImageSwipe } from '../components'
+import { ImageSwipe } from '../components';
 
 // BUG / TODO:
 // - doesnt show first or last (not sure which) setup to like
@@ -16,7 +16,6 @@ const Swipe = () => {
   const [balanceSetups, setBalanceSetups] = useState([]);
 
   useEffect(() => {
-
     // get user data
     fetch(`http://localhost:8080/api/user/id`, {
       method: `GET`,
@@ -31,7 +30,6 @@ const Swipe = () => {
         console.error(e);
       });
 
-
     fetch(`http://localhost:8080/api/user/${userId}`, {
       method: `GET`,
       credentials: `include`,
@@ -45,91 +43,90 @@ const Swipe = () => {
       .catch((e) => {
         console.error(e);
       });
-
-
   }, [userId]);
 
   useEffect(() => {
-
-    // run once on render (mount), to fetch all setups 
+    // run once on render (mount), to fetch all setups
     fetch('http://localhost:8080/api/setup')
-    .then(
-      (data) => data.json(),
-      (err) => console.log(err)
-    )
-    .then(
-      (parsedData) => {
+      .then(
+        (data) => data.json(),
+        (err) => console.log(err)
+      )
+      .then(
+        (parsedData) => {
+          // create temp array to store all setups
+          let tempSetupsArray = parsedData;
 
-        // create temp array to store all setups
-        let tempSetupsArray = parsedData;
+          console.log(`parsedData1111`, parsedData);
 
-        console.log(`parsedData1111`, parsedData);
+          console.log(`userId`, userId);
 
-        console.log(`userId`, userId)
+          // BUG / TODO:
+          // trying to filter away setups that user already swiped.
+          // doesn't work on page load. but after loading page, save the Swipe.jsx file in code editor, and should see frontend update with some setups removed from the balanceSetups.
 
-        // BUG / TODO:
-        // trying to filter away setups that user already swiped.
-        // doesn't work on page load. but after loading page, save the Swipe.jsx file in code editor, and should see frontend update with some setups removed from the balanceSetups.
+          // TODO:
+          //  if (user rated this setup before) {
+          //   remove the item and repeat to pick next random item;
+          // } else {
+          //   show this setup to user;
+          // }
 
-        // TODO:
-        //  if (user rated this setup before) {
-        //   remove the item and repeat to pick next random item;
-        // } else {
-        //   show this setup to user;
-        // }
-      
-        // loop through the parsedData (all setups)
-        for (let i=0; i<parsedData.length; i++) {
-          console.log(`in the loop, ${parsedData[i]}`)
-          // for each indiivdual setup, loop through the array for all swipes
-          for (let j=0; j<parsedData[i].swipes.length; j++) {
-            // check if the currently logged in userId === the userId in one of the swiped items
-            if (userId === parsedData[i].swipes[j].userId) {
-              console.log(`found that this setup ${parsedData[i].title} is already swiped!`)
-              // remove this item from the tempSetupsArray
-              console.log(`gonna remove this item`, tempSetupsArray[i].swipes[j]);
-              console.log(`gonna remove this setup`, tempSetupsArray[i]);
-              // tempSetupsArray[i].swipes.splice(j);
-              console.log(`removedSwipe:`, tempSetupsArray.splice(i, 1));
+          // loop through the parsedData (all setups)
+          for (let i = 0; i < parsedData.length; i++) {
+            console.log(`in the loop, ${parsedData[i]}`);
+            // for each indiivdual setup, loop through the array for all swipes
+            for (let j = 0; j < parsedData[i].swipes.length; j++) {
+              // check if the currently logged in userId === the userId in one of the swiped items
+              if (userId === parsedData[i].swipes[j].userId) {
+                console.log(
+                  `found that this setup ${parsedData[i].title} is already swiped!`
+                );
+                // remove this item from the tempSetupsArray
+                console.log(
+                  `gonna remove this item`,
+                  tempSetupsArray[i].swipes[j]
+                );
+                console.log(`gonna remove this setup`, tempSetupsArray[i]);
+                // tempSetupsArray[i].swipes.splice(j);
+                console.log(`removedSwipe:`, tempSetupsArray.splice(i, 1));
 
-              break;
-
+                break;
+              }
             }
           }
-        }
 
-        console.log(`tempSetupsArray after for loop spaghetti`, tempSetupsArray);
+          console.log(
+            `tempSetupsArray after for loop spaghetti`,
+            tempSetupsArray
+          );
 
-        // randomly pick 1 setup (removes item from tempArray)
-        let randomIndex = Math.floor(Math.random() * parsedData.length);
-        let removedItem = tempSetupsArray.splice(randomIndex, 1); // returns array with 1 item
+          // randomly pick 1 setup (removes item from tempArray)
+          let randomIndex = Math.floor(Math.random() * parsedData.length);
+          let removedItem = tempSetupsArray.splice(randomIndex, 1); // returns array with 1 item
 
-        console.log(`parsedData selectedItem`, removedItem);
+          console.log(`parsedData selectedItem`, removedItem);
 
+          console.log(
+            `parsedData selectedItem[0].swipes`,
+            removedItem[0].swipes
+          );
 
-        console.log(`parsedData selectedItem[0].swipes`, removedItem[0].swipes);
+          // store randomly picked item as currentSetup to show on UI
+          setCurrentSetup(removedItem[0]);
 
+          // store remainder array (less removedItem) to balanceSetups state
+          setBalanceSetups(tempSetupsArray);
+        },
+        (err) => console.log(err)
+      );
+  }, []);
 
-        // store randomly picked item as currentSetup to show on UI
-        setCurrentSetup(removedItem[0]);
-
-        // store remainder array (less removedItem) to balanceSetups state
-        setBalanceSetups(tempSetupsArray);
-
-
-      },
-      (err) => console.log(err)
-    );
-      
-  }, [])
-    
   // handle user click like button or swipe right on mobile
   const handleLiked = (bool) => {
-
     console.log(`Swiped Setup`, currentSetup);
-    
-    if (balanceSetups.length > 0) {
 
+    if (balanceSetups.length > 0) {
       // update setup in db
       fetch(`http://localhost:8080/api/setup/${currentSetup._id}`, {
         method: 'PUT',
@@ -137,55 +134,80 @@ const Swipe = () => {
         body: JSON.stringify({
           // push new swipe object to the swipes array of this setup
           $push: {
-            "swipes": {
-              "userId": userId,
-              "liked": bool,
-              $currentDate: { 
-                "lastModified": true,
-                $type: "timestamp",
+            swipes: {
+              userId: userId,
+              liked: bool,
+              $currentDate: {
+                lastModified: true,
+                $type: 'timestamp',
               },
             },
           },
-        })
+        }),
       })
-      .then((res) => res.json())
-      .then((updatedSetup) => {
+        .then((res) => res.json())
+        .then((updatedSetup) => {
+          console.log(
+            `updatedSetup`,
+            updatedSetup.swipes[updatedSetup.swipes.length - 1]
+          );
 
-        console.log(`updatedSetup`, updatedSetup.swipes[updatedSetup.swipes.length-1])
+          console.log(
+            `Update Setup id ${updatedSetup._id} (${
+              updatedSetup.title
+            }) | by UserId ${
+              updatedSetup.swipes[updatedSetup.swipes.length - 1].userId
+            } (${userEmail}) | LIKED? ${
+              updatedSetup.swipes[updatedSetup.swipes.length - 1].liked
+            } | timestamp: ${
+              updatedSetup.swipes[updatedSetup.swipes.length - 1].timestamp
+            }`
+          );
 
-        console.log(`Update Setup id ${updatedSetup._id} (${updatedSetup.title}) | by UserId ${updatedSetup.swipes[updatedSetup.swipes.length-1].userId} (${userEmail}) | LIKED? ${updatedSetup.swipes[updatedSetup.swipes.length-1].liked} | timestamp: ${updatedSetup.swipes[updatedSetup.swipes.length-1].timestamp}`);
-
-        // then also update user in db
-        fetch(`http://localhost:8080/api/user/${userId}`, {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            // push new swipe object to the swipedSetups array of this user
-            $push: {
-              "swipedSetups": {
-                "setupId": updatedSetup._id,
-                "liked": bool,
-                $currentDate: { 
-                  "lastModified": true,
-                  $type: "timestamp",
+          // then also update user in db
+          fetch(`http://localhost:8080/api/user/${userId}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              // push new swipe object to the swipedSetups array of this user
+              $push: {
+                swipedSetups: {
+                  setupId: updatedSetup._id,
+                  liked: bool,
+                  $currentDate: {
+                    lastModified: true,
+                    $type: 'timestamp',
+                  },
                 },
               },
-            },
+            }),
           })
-        })
-        .then((res) => res.json())
-        .then((updatedUser) => {
+            .then((res) => res.json())
+            .then((updatedUser) => {
+              console.log(`updatedUser`.updatedUser);
+              console.log(
+                `updatedUser swipedSetups`,
+                updatedUser.swipedSetups[updatedUser.swipedSetups.length - 1]
+              );
 
-          console.log(`updatedUser`. updatedUser)
-          console.log(`updatedUser swipedSetups`, updatedUser.swipedSetups[updatedUser.swipedSetups.length-1])
-
-          console.log(`Update User id ${updatedUser._id} (${updatedUser.email}) | Swiped ${updatedUser.swipedSetups[updatedUser.swipedSetups.length-1].setupId} | LIKED? ${updatedUser.swipedSetups[updatedUser.swipedSetups.length-1].liked} | timestamp: ${updatedUser.swipedSetups[updatedUser.swipedSetups.length-1].timestamp}`);
-
+              console.log(
+                `Update User id ${updatedUser._id} (${
+                  updatedUser.email
+                }) | Swiped ${
+                  updatedUser.swipedSetups[updatedUser.swipedSetups.length - 1]
+                    .setupId
+                } | LIKED? ${
+                  updatedUser.swipedSetups[updatedUser.swipedSetups.length - 1]
+                    .liked
+                } | timestamp: ${
+                  updatedUser.swipedSetups[updatedUser.swipedSetups.length - 1]
+                    .timestamp
+                }`
+              );
+            })
+            .catch((err) => console.error({ error: err }));
         })
         .catch((err) => console.error({ error: err }));
-
-      })
-      .catch((err) => console.error({ error: err }));
 
       // handle updating balanceSetups and currentSetup states after Liking
 
@@ -198,31 +220,37 @@ const Swipe = () => {
       setCurrentSetup(removedItem[0]);
       // store remainder array (less removedItem) to balanceSetups state
       setBalanceSetups(tempBalanceArray);
-
     } else {
-      console.log("no more new photos to show :( try again later");
+      console.log('no more new photos to show :( try again later');
       setBalanceSetups([]);
     }
-    
   };
 
-
-      
   return (
     <>
-    <h1 className="mt-[8px] font-bold md:text-[40px] text-[28px] text-white text-center">Swipe Right to Like</h1>
-    <h2 className="mt-[8px] font-normal sm:text-[28px] text-[18px] text-center text-secondary-white  mb-6">Curate your list of favourite desk setups</h2>
-    <Container p="10px">
-      { 
-        (balanceSetups.length > 0) ? (
-          <ImageSwipe src={currentSetup.img} handleLiked={handleLiked} />
+      <h1 className='mt-[8px] font-bold md:text-[40px] text-[28px] text-white text-center'>
+        Swipe Right to Like
+      </h1>
+      <h2 className='mt-[8px] font-normal sm:text-[28px] text-[18px] text-center text-secondary-white  mb-6'>
+        Curate your list of favourite desk setups
+      </h2>
+      <Container p='10px'>
+        {balanceSetups.length > 0 ? (
+          <ImageSwipe
+            src={currentSetup.img}
+            title={currentSetup.title}
+            user={currentSetup.user}
+            handleLiked={handleLiked}
+          />
         ) : (
-          <h4 className="text-secondary-white text-center">That's all folks! No more desks for you to judge – time to get back to your own cluttered workspace!</h4>
-        )
-      }
-    </Container>
+          <h4 className='text-secondary-white text-center'>
+            That's all folks! No more desks for you to judge – time to get back
+            to your own cluttered workspace!
+          </h4>
+        )}
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Swipe
+export default Swipe;
