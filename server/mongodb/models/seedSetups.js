0b007f7190eb9b7f03f9b75d3b5ae48cbe5650c9
deeba <adibah@hotmail.sg>
2023-03-29 23:16:45 +0800

    fix: home show setup from database

--- server/mongodb/models/seedSetups.js
+++ server/mongodb/models/seedSetups.js
@@ -1,23 +1,165 @@
 const setups = [
   {
     img: 'https://t3.gstatic.com/images?q=tbn:ANd9GcSTspk0HexUgLVvH7AoWUOfZ61RecRVeAaoubWnCQHmAfVY9rKB',
+    userId: '1',
     user: 'Alice',
-    products: ['Desk', 'Monitor', 'Keyboard', 'Mouse', 'Chair'],
+    title: 'Blue Battlestation',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
+    products: [
+      {
+        type: 'keyboard',
+        brand: 'Keychron',
+        model: 'K4',
+      },
+      {
+        type: 'mouse',
+        brand: 'Razer',
+        model: 'Deathadder V2',
+      },
+      {
+        type: 'monitor',
+        brand: 'Xiaomi',
+        model: 'Curved Gaming Monitor 34',
+      },
+    ],
+    swipes: [
+      {
+        userId: '23423',
+        liked: true,
+      },
+    ],
   },
   {
     img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT13ujza2Re0zLk0Vk5VviScLa1zYzu0VGxdW1igIYd63UDxtJA',
+    userId: '131232',
     user: 'Bob',
-    products: ['Desk', 'Monitor', 'Keyboard', 'Mouse', 'Mousepad', 'Chair'],
+    title: 'Mellow Woods',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
+    products: [
+      {
+        type: 'keyboard',
+        brand: 'Keychron',
+        model: 'K4',
+      },
+      {
+        type: 'mouse',
+        brand: 'Razer',
+        model: 'Deathadder V2',
+      },
+      {
+        type: 'monitor',
+        brand: 'Xiaomi',
+        model: 'Curved Gaming Monitor 34',
+      },
+      {
+        type: 'mousepad',
+        brand: 'Aukey',
+        model: 'KM-P2 Large Gaming Mouse Pad Oversized',
+      },
+      {
+        type: 'chair',
+        brand: 'Logitech X Herman Miller',
+        model: 'Embody gaming chair',
+      },
+      {
+        type: 'light',
+        brand: 'Type 75 Desk Lamp Paul Smith',
+        model: 'Edition 6',
+      },
+    ],
+    swipes: [
+      {
+        userId: '1',
+        liked: true,
+      },
+    ],
   },
   {
     img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSKdjQk306iqnHH4Ews6MqBkTkbgWIPBT0JTrC3-jgO5wWhR0ck',
+    userId: '13131',
     user: 'Charlie',
-    products: ['Desk', 'Monitor', 'Speaker', 'PC', 'Keyboard', 'Mousepad'],
+    title: 'Coding Club',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
+    products: [
+      {
+        type: 'monitor',
+        brand: 'Xiaomi',
+        model: 'Curved Gaming Monitor 34',
+      },
+      {
+        type: 'mousepad',
+        brand: 'Aukey',
+        model: 'KM-P2 Large Gaming Mouse Pad Oversized',
+      },
+      {
+        type: 'chair',
+        brand: 'Logitech X Herman Miller',
+        model: 'Embody gaming chair',
+      },
+      {
+        type: 'light',
+        brand: 'Type 75 Desk Lamp Paul Smith',
+        model: 'Edition 6',
+      },
+    ],
+    swipes: [
+      {
+        userId: '131232',
+        liked: true,
+      },
+      {
+        userId: '4234',
+        liked: true,
+      },
+    ],
   },
   {
     img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ7Y3DJGIZejTxIdwspZPJhd40NKcYKHKXbGuoH7MxteDaJZJQl',
+    userId: '4234',
     user: 'Delilah',
-    products: ['Desk', 'Monitor', 'Monitor', 'Speaker', 'Mouse'],
+    title: 'Autumn Dev',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
+    products: [
+      {
+        type: 'monitor',
+        brand: 'Gigabyte',
+        model: 'M34WQ',
+      },
+      {
+        type: 'mousepad',
+        brand: 'Aukey',
+        model: 'KM-P2 Large Gaming Mouse Pad Oversized',
+      },
+      {
+        type: 'chair',
+        brand: 'Logitech X Herman Miller',
+        model: 'Embody gaming chair',
+      },
+      {
+        type: 'light',
+        brand: 'Type 75 Desk Lamp Paul Smith',
+        model: 'Edition 6',
+      },
+      {
+        type: 'speaker',
+        brand: 'Audioengine',
+        model: 'Audioengine A2+ Wireless White',
+      },
+    ],
+    swipes: [
+      {
+        userId: '131232',
+        liked: false,
+      },
+      {
+        userId: '1',
+        liked: true,
+      },
+    ],
   },
   {
     img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTNQpmQeZUnFRTl08aJyEAcAFNuNeLFo_yIIWDWWMWFWntKSgW5',
--- server/routes/setupRoutes.js
+++ server/routes/setupRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { v2 as cloudinary } from 'cloudinary';
+import {v2 as cloudinary} from 'cloudinary';
 
 import setup from '../mongodb/models/setup.js';
 import requireAuth from '../auth/authMiddleware.js';
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
     const currentSetup = await setup.find({});
     res.status(200).send(currentSetup);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({error: err.message});
   }
 });
 
@@ -27,7 +27,7 @@ router.post('/', async (req, res) => {
   console.log(`hello from the /api/setup post route!`);
   // console.log(req.body);
   try {
-    const { img, userId } = req.body;
+    const {img, userId} = req.body;
     const photoUrl = await cloudinary.uploader.upload(img);
     const newPost = {
       ...req.body,
@@ -40,13 +40,13 @@ router.post('/', async (req, res) => {
     const createdSetup = await setup.create(newPost);
     res.status(200).send(createdSetup);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({error: err.message});
   }
 });
 
 // To send the seed files to DB
-// 1) comment out top router.post (line 15 to 23)
-// 2) uncomment below router.post (line 31 to 72)
+// 1) comment out top router.post (line 26 to 45)
+// 2) uncomment below router.post (line 53 to 225)
 // 3) to save to seed to the DB => got to create page and click submit
 // 4) comment line 30 to 67 out after done.
 // 5) uncomment out top router.post (line 15 to 23) and save.
@@ -56,40 +56,170 @@ router.post('/', async (req, res) => {
 //     const createdSetup = await setup.create(
 //       {
 //         img: 'https://t3.gstatic.com/images?q=tbn:ANd9GcSTspk0HexUgLVvH7AoWUOfZ61RecRVeAaoubWnCQHmAfVY9rKB',
+//         userId: '1',
 //         user: 'Alice',
 //         title: 'Blue Battlestation',
 //         description:
 //           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
-//         type: ['desk', 'monitor', 'keyboard', 'mouse', 'chair'],
+//         products: [
+//           {
+//             type: 'keyboard',
+//             brand: 'Keychron',
+//             model: 'K4',
+//           },
+//           {
+//             type: 'mouse',
+//             brand: 'Razer',
+//             model: 'Deathadder V2',
+//           },
+//           {
+//             type: 'monitor',
+//             brand: 'Xiaomi',
+//             model: 'Curved Gaming Monitor 34',
+//           },
+//         ],
+//         swipes: [
+//           {
+//             userId: '23423',
+//             liked: true,
+//           },
+//         ],
 //       },
 //       {
 //         img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT13ujza2Re0zLk0Vk5VviScLa1zYzu0VGxdW1igIYd63UDxtJA',
+//         userId: '131232',
 //         user: 'Bob',
 //         title: 'Mellow Woods',
 //         description:
 //           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
-//         type: ['desk', 'monitor', 'keyboard', 'mouse', 'mousepad', 'chair'],
+//         products: [
+//           {
+//             type: 'keyboard',
+//             brand: 'Keychron',
+//             model: 'K4',
+//           },
+//           {
+//             type: 'mouse',
+//             brand: 'Razer',
+//             model: 'Deathadder V2',
+//           },
+//           {
+//             type: 'monitor',
+//             brand: 'Xiaomi',
+//             model: 'Curved Gaming Monitor 34',
+//           },
+//           {
+//             type: 'mousepad',
+//             brand: 'Aukey',
+//             model: 'KM-P2 Large Gaming Mouse Pad Oversized',
+//           },
+//           {
+//             type: 'chair',
+//             brand: 'Logitech X Herman Miller',
+//             model: 'Embody gaming chair',
+//           },
+//           {
+//             type: 'light',
+//             brand: 'Type 75 Desk Lamp Paul Smith',
+//             model: 'Edition 6',
+//           },
+//         ],
+//         swipes: [
+//           {
+//             userId: '1',
+//             liked: true,
+//           },
+//         ],
 //       },
 //       {
 //         img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSKdjQk306iqnHH4Ews6MqBkTkbgWIPBT0JTrC3-jgO5wWhR0ck',
+//         userId: '13131',
 //         user: 'Charlie',
 //         title: 'Coding Club',
 //         description:
 //           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
-//         type: ['desk', 'monitor', 'speaker', 'pc', 'keyboard', 'mousepad'],
+//         products: [
+//           {
+//             type: 'monitor',
+//             brand: 'Xiaomi',
+//             model: 'Curved Gaming Monitor 34',
+//           },
+//           {
+//             type: 'mousepad',
+//             brand: 'Aukey',
+//             model: 'KM-P2 Large Gaming Mouse Pad Oversized',
+//           },
+//           {
+//             type: 'chair',
+//             brand: 'Logitech X Herman Miller',
+//             model: 'Embody gaming chair',
+//           },
+//           {
+//             type: 'light',
+//             brand: 'Type 75 Desk Lamp Paul Smith',
+//             model: 'Edition 6',
+//           },
+//         ],
+//         swipes: [
+//           {
+//             userId: '131232',
+//             liked: true,
+//           },
+//           {
+//             userId: '4234',
+//             liked: true,
+//           },
+//         ],
 //       },
 //       {
 //         img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ7Y3DJGIZejTxIdwspZPJhd40NKcYKHKXbGuoH7MxteDaJZJQl',
+//         userId: '4234',
 //         user: 'Delilah',
 //         title: 'Autumn Dev',
 //         description:
 //           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis delectus nihil odio.',
-//         type: ['desk', 'monitor', 'speaker', 'mouse'],
+//         products: [
+//           {
+//             type: 'monitor',
+//             brand: 'Gigabyte',
+//             model: 'M34WQ',
+//           },
+//           {
+//             type: 'mousepad',
+//             brand: 'Aukey',
+//             model: 'KM-P2 Large Gaming Mouse Pad Oversized',
+//           },
+//           {
+//             type: 'chair',
+//             brand: 'Logitech X Herman Miller',
+//             model: 'Embody gaming chair',
+//           },
+//           {
+//             type: 'light',
+//             brand: 'Type 75 Desk Lamp Paul Smith',
+//             model: 'Edition 6',
+//           },
+//           {
+//             type: 'speaker',
+//             brand: 'Audioengine',
+//             model: 'Audioengine A2+ Wireless White',
+//           },
+//         ],
+//         swipes: [
+//           {
+//             userId: '131232',
+//             liked: false,
+//           },
+//           {
+//             userId: '1',
+//             liked: true,
+//           },
+//         ],
 //       }
 //     );
 //     res.status(200).send(createdSetup);
 //   } catch (err) {
-//     res.status(400).json({ error: err.message });
+//     res.status(400).json({error: err.message});
 //   }
 // });
 
@@ -99,7 +229,7 @@ router.get('/:id', async (req, res) => {
     const selectedSetup = await setup.findById(req.params.id);
     res.status(200).send(selectedSetup);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({error: err.message});
   }
 });
 
@@ -109,7 +239,7 @@ router.delete('/:id', async (req, res) => {
     const deletedSetup = await setup.findByIdAndRemove(req.params.id);
     res.status(200).send(deletedSetup);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({error: err.message});
   }
 });
 // router.delete('/:id', requireAuth, async (req, res) => {
@@ -138,11 +268,11 @@ router.put('/:id', async (req, res) => {
     const updatedSetup = await setup.findByIdAndUpdate(
       req.params.id,
       req.body,
-      { new: true } // to return document after `update` was applied
+      {new: true} // to return document after `update` was applied
     );
     res.status(200).send(updatedSetup);
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    res.status(400).json({error: err.message});
   }
 });
 
