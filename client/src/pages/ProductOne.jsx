77034a21646b54d667ae9e8dc87127c9bb31d5ba
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-30 18:28:42 +0800

    feat: show type and features on product details pg

--- client/src/pages/ProductOne.jsx
+++ client/src/pages/ProductOne.jsx
@@ -5,15 +5,16 @@ import { DetailsProduct } from '../components';
 
 const ProductOne = () => {
   const location = useLocation();
-  const { img, brand, model, ratings, reviews } = location.state;
+  const { img, type, brand, model, ratings, reviews } = location.state;
 
   return (
     <div>
-      <h1 className="mt-[8px] font-bold md:text-[40px] text-[28px] text-white text-center">Product</h1>
-      <h2 className="mt-[8px] font-normal sm:text-[28px] text-[18px] text-center text-secondary-white  mb-6">This is a product</h2>
+      <h1 className="mt-[8px] font-bold md:text-[40px] text-[28px] text-white text-center">{brand} - {model}</h1>
+      <h2 className="mt-[8px] font-normal sm:text-[28px] text-[18px] text-center text-secondary-white  mb-6">{type}</h2>
       <div>
         <DetailsProduct
           img={img}
+          type={type}
           brand={brand}
           model={model}
           ratings={ratings}
